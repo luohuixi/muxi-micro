943748fa925714c66c2e24d0332be5a22bc97441
sunjunnan <2101763682@qq.com>
2025-06-26 00:14:12 +0800

    feat:基本完成logger部分

---
+++ example/logger/logger.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"github.com/muxi-Infra/muxi-micro/pkg/errs"
+	"github.com/muxi-Infra/muxi-micro/pkg/logger"
+	"github.com/muxi-Infra/muxi-micro/pkg/logger/zapx"
+)
+
+var DBNOData = errs.NewErr("db_no_data", "db has no data")
+
+var UserNotFound = errs.NewErr("user_not_found", "User not found")
+
+func SearchDB(id int) error {
+	return DBNOData.WithCause(errors.New("row is 0"))
+}
+
+func SearchUser(id int) error {
+	err := SearchDB(id)
+	if errors.Is(err, DBNOData) {
+		return UserNotFound.WithCause(err).WithMeta(map[string]interface{}{
+			"user_id": id,
+		})
+	}
+	return nil
+}
+
+func main() {
+	l := zapx.NewDefaultZapLogger(logger.EnvTest, true, "./logs")
+	id := 1
+	A(id, l)
+	l.Info("查询出错id:", id)
+}
+
+func A(id int, l logger.Logger) {
+	err := SearchUser(id)
+	l.Error("查询数据库出错:", err)
+}
